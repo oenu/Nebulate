78a1edc70513781c6f3eeba1f1e1f9dcd7ee0a62
oenu <51684443+oenu@users.noreply.github.com>
2022-06-21 14:17:35 -0400

    replaced file search with global value

--- src/Functions/videosFromNebula.ts
+++ src/Functions/videosFromNebula.ts
@@ -17,12 +17,6 @@ export const videosFromNebula = async (
     videoScrapeLimit = 100;
   }
 
-  // Get the token from the file
-  const token: string = await fs.promises.readFile(
-    path.join(__dirname, "..", "store", "json_token.txt"),
-    "utf8"
-  );
-
   for (let scrapedVideos = 0; scrapedVideos < videoScrapeLimit; ) {
     try {
       const url = `https://content.watchnebula.com/video/channels/${creatorSlug}/`;
@@ -30,7 +24,7 @@ export const videosFromNebula = async (
 
       const response = await axios.get(requestUrl, {
         data: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${global.token}`,
         },
       });
 

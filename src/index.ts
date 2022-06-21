42dac723f5085b411ff2c97c6371487e159e2a86
oenu <51684443+oenu@users.noreply.github.com>
2022-06-21 13:29:00 -0400

    global vars, var inits, auth middleware

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ import "dotenv/config";
 // Constants
 const port = process.env.PORT || 3000;
 
+// Types
+declare global {
+  var token: string;
+  var key: string;
+}
+
+// Token store initialization
+
 // Mongoose
 import mongoose from "mongoose";
 import { connectDB } from "./config/dbConfig";
@@ -13,6 +21,9 @@ connectDB();
 
 // Middleware
 import logger from "./config/logger";
+import auth from "./middleware/auth";
+import globalInit from "./store/store";
+app.use(auth);
 
 // Routes
 app.get("/", (_req, res: Response) => {
@@ -28,7 +39,9 @@ app.get("/update/:creatorSlug", async (req: Request, res: Response) => {
 });
 
 // Start the server
-mongoose.connection.once("open", () => {
+mongoose.connection.once("open", async () => {
+  // Initialize global variables
+  await globalInit();
   console.log("Connected to MongoDB");
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);

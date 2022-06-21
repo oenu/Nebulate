f58e322cf4a7f57a1687a3722f33e0acde5b0933
oenu <51684443+oenu@users.noreply.github.com>
2022-06-21 15:46:52 -0400

    mongodb init

---
+++ src/Functions/sendNewVideosToDatabase.ts
@@ -0,0 +1,6 @@
+// Send new videos to MongoDB
+
+import mongoose from "mongoose";
+import { Video } from "../models/video";
+
+const sendNewVideosToDatabase = async (videos: any, creatorSlug: string) => {};

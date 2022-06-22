d1f19d3e9aa9662d6646a29e8396058a73ae8430
oenu <51684443+oenu@users.noreply.github.com>
2022-06-22 14:54:58 -0400

    init youtube video model and schema

---
+++ src/models/nebulaVideo.ts
@@ -0,0 +1,56 @@
+// Video type and model
+import mongoose from "mongoose";
+import { Schema, InferSchemaType } from "mongoose";
+
+const nebulaVideoSchema = new Schema(
+  {
+    id: {
+      type: "String",
+    },
+    slug: {
+      type: "String",
+      index: true,
+    },
+    title: {
+      type: "String",
+    },
+    short_description: {
+      type: "String",
+    },
+    duration: {
+      type: "Number",
+    },
+    published_at: {
+      type: "Date",
+    },
+    channel_id: {
+      type: "String",
+    },
+    channel_slug: {
+      type: "String",
+      index: true,
+    },
+    channel_slugs: {
+      type: ["String"],
+    },
+    channel_title: {
+      type: "String",
+    },
+    share_url: {
+      type: "String",
+    },
+    channel: {
+      type: "Mixed",
+    },
+  },
+  {
+    collection: "nebulaVideos",
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+export type NebulaVideo = InferSchemaType<typeof nebulaVideoSchema>;
+export const NebulaVideo = mongoose.model<NebulaVideo>(
+  "NebulaVideo",
+  nebulaVideoSchema
+);

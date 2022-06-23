be1e34fb1c380e0d285d534c4a82c2993223c90c
oenu <51684443+oenu@users.noreply.github.com>
2022-06-22 21:59:14 -0400

    get data from database

--- src/Functions/matchVideos.ts
+++ src/Functions/matchVideos.ts
@@ -1,5 +1,10 @@
 import logger from "../config/logger";
 import { Creator } from "../models/creator";
+import videosFromNebula from "./videosFromNebula";
+import videosFromYoutube from "./videosFromYoutube";
+import { YoutubeVideo as YoutubeVideos } from "../models/youtubeVideo";
+import { NebulaVideo as NebulaVideos } from "../models/nebulaVideo";
+
 /**
  * Match Nebula videos to Youtube videos, from the database
  * @param  {string} channel_slug - The channel slug
@@ -8,8 +13,8 @@ import { Creator } from "../models/creator";
  */
 const matchVideos = async (
   channel_slug: string,
-  rematch_nebula_slug?: string,
-  rematch_yt_id?: string
+  rematch_nebula_slug?: Array<string>,
+  rematch_yt_id?: Array<string>
 ) => {
   logger.log(`Matching videos for ${channel_slug}`);
   console.log(rematch_nebula_slug, rematch_yt_id); // Temp
@@ -29,5 +34,49 @@ const matchVideos = async (
     throw new Error(`Register: Creator ${channel_slug} not found in DB`);
   }
   console.log(creator); // Temp
-  // Get videos
+
+  // Check the last time the creator's nebula videos were scraped
+  const { last_scraped_nebula, last_scraped_youtube } = creator;
+
+  // If the creator's videos were scraped more than 4 hours ago, scrape them again
+  if (
+    last_scraped_nebula &&
+    last_scraped_youtube &&
+    new Date().getTime() -
+      Math.min(last_scraped_nebula.getTime(), last_scraped_youtube.getTime()) >
+      14400000
+  ) {
+    logger.log("Match: Creator scraped more than 4 hours ago, scraping again");
+    try {
+      await videosFromNebula(channel_slug, true);
+      await videosFromYoutube(channel_slug, true);
+    } catch (error) {
+      logger.error("Match: Error scraping videos");
+      throw error;
+    }
+  }
+
+  // Get creator's youtube videos
+  const youtube_videos = await YoutubeVideos.find({
+    _id: {
+      $in: creator.youtube_videos?.map((video: any) => {
+        return video._id;
+      }),
+    },
+  });
+  if (!youtube_videos) {
+    throw new Error(`Match: No youtube videos found for ${channel_slug}`);
+  }
+
+  // Get creator's nebula videos
+  const nebula_videos = await NebulaVideos.find({
+    _id: {
+      $in: creator.nebula_videos?.map((video: any) => {
+        return video._id;
+      }),
+    },
+  });
+  if (!nebula_videos) {
+    throw new Error(`Match: No nebula videos found for ${channel_slug}`);
+  }
 };

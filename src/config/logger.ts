d4803a3ffc80a1340b65a089a07bf968bb1e17dc
oenu <51684443+oenu@users.noreply.github.com>
2022-06-22 20:56:53 -0400

    prettier logging

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -3,18 +3,21 @@ const winston = require("winston");
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
+  // defaultMeta: { service: "user-service" },
   transports: [
     new winston.transports.File({
+      format: winston.format.json(),
       filename: path.join(__dirname, "..", "/logs", "info.log"),
       level: "info",
     }),
     new winston.transports.File({
+      format: winston.format.json(),
       filename: path.join(__dirname, "..", "/logs", "error.log"),
       level: "error",
     }),
     new winston.transports.File({
+      format: winston.format.json(),
+      timestamp: true,
       filename: path.join(__dirname, "..", "/logs", "combined.log"),
     }),
   ],
@@ -24,8 +27,10 @@ if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
-        winston.format.prettyPrint(),
-        winston.format.colorize()
+        winston.format.colorize(),
+        winston.format.splat(),
+        winston.format.simple()
+        // winston.format.prettyPrint(),
       ),
     })
   );

c2c7a34b3664d9e8c15200c3c839e967d66ddec9
oenu <51684443+oenu@users.noreply.github.com>
2022-06-22 21:26:56 -0400

    added more comment examples and updated schemas

--- src/models/nebulaVideo.ts
+++ src/models/nebulaVideo.ts
@@ -2,46 +2,72 @@
 import mongoose from "mongoose";
 import { Schema, InferSchemaType } from "mongoose";
 
+/**
+ * nebulaVideoSchema schema
+ * @constructor NebulaVideo
+ */
 const nebulaVideoSchema = new Schema(
   {
     id: {
+      // "video_episode:d49e13df-f1ed-4562-8209-6098de1e187f"
       type: "String",
       index: true,
     },
     slug: {
+      // "real-lawyer-reacts-to-will-smith-slapping-chris-rock"
       type: "String",
       index: true,
     },
     title: {
+      // "Real Lawyer Reacts to Will Smith Slapping Chris Rock"
       type: "String",
     },
     short_description: {
+      // "Welcome to earth, Chris Rock."
       type: "String",
     },
     duration: {
+      // 568
       type: "Number",
     },
     published_at: {
+      // "2022-03-29T16:58:40Z"
       type: "Date",
     },
     channel_id: {
+      // video_channel:85bf1f47-7cb1-409f-ae8f-4ea1a9b4414b
       type: "String",
     },
     channel_slug: {
+      // "legaleagle"
       type: "String",
       index: true,
     },
     channel_slugs: {
+      // ["legaleagle"]
       type: ["String"],
     },
     channel_title: {
+      // "LegalEagle"
       type: "String",
     },
     share_url: {
+      // "https://nebula.app/videos/legaleagle-real-lawyer-reacts-to-will-smith-slapping-chris-rock/"
       type: "String",
     },
-    channel: {
-      type: "Mixed",
+    matched: {
+      // false
+      type: "Boolean",
+      default: false,
+    },
+    youtubeVideo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "YoutubeVideo",
+    },
+    youtubeVideoId: {
+      // "PtxNsc85KMw"
+      type: "String",
+      index: true,
     },
   },
   {

c77a10d3ed3c3a761813a44a30c15c6131e81404
oenu <51684443+oenu@users.noreply.github.com>
2022-06-22 22:26:22 -0400

    added fetch methods for YT and Nebula

--- src/Functions/matchVideos.ts
+++ src/Functions/matchVideos.ts
@@ -8,16 +8,19 @@ import { NebulaVideo as NebulaVideos } from "../models/nebulaVideo";
 /**
  * Match Nebula videos to Youtube videos, from the database
  * @param  {string} channel_slug - The channel slug
- * @param  {string} rematch_nebula_slug? - The slug of the video to rematch
- * @param  {string} rematch_yt_id? - The youtube id of the video to rematch
+ * @param  {Array.<string>} rematch_nebula_slug? - The slug of the video to rematch
+ * @param  {Array.<string>} rematch_yt_id? - The youtube id of the video to rematch
+ * @param {boolean} rematch_all? - Whether to attempt to rematch all videos
+ *
  */
 const matchVideos = async (
   channel_slug: string,
   rematch_nebula_slug?: Array<string>,
-  rematch_yt_id?: Array<string>
+  rematch_yt_id?: Array<string>,
+  rematch_all?: boolean
 ) => {
   logger.log(`Matching videos for ${channel_slug}`);
-  console.log(rematch_nebula_slug, rematch_yt_id); // Temp
+  console.log(rematch_nebula_slug, rematch_yt_id, rematch_all); // Temp
 
   // Check for creator slug
   try {
@@ -57,26 +60,62 @@ const matchVideos = async (
   }
 
   // Get creator's youtube videos
-  const youtube_videos = await YoutubeVideos.find({
-    _id: {
-      $in: creator.youtube_videos?.map((video: any) => {
-        return video._id;
-      }),
-    },
-  });
-  if (!youtube_videos) {
-    throw new Error(`Match: No youtube videos found for ${channel_slug}`);
+  let youtube_videos: YoutubeVideos[] = [];
+  // Get specific youtube video/videos if passed in
+  if (rematch_yt_id && !rematch_all) {
+    const specificYtVideos = await YoutubeVideos.find({
+      youtube_id: { $in: rematch_yt_id },
+    });
+    if (specificYtVideos && specificYtVideos.length > 0) {
+      youtube_videos.push(...specificYtVideos);
+    } else {
+      throw new Error(`Match: Youtube video ${rematch_yt_id} not found in DB`);
+    }
+  }
+
+  // Get all youtube videos if no specific video was passed in
+  if (!rematch_yt_id || rematch_all) {
+    youtube_videos = await YoutubeVideos.find({
+      _id: {
+        $in: creator.youtube_videos?.map((video: any) => {
+          return video._id;
+        }),
+      },
+    }).select("title");
+    if (!youtube_videos) {
+      throw new Error(`Match: No youtube videos found for ${channel_slug}`);
+    }
+    logger.info(`Match: Found ${youtube_videos.length} youtube videos`);
   }
 
   // Get creator's nebula videos
-  const nebula_videos = await NebulaVideos.find({
-    _id: {
-      $in: creator.nebula_videos?.map((video: any) => {
-        return video._id;
-      }),
-    },
-  });
-  if (!nebula_videos) {
-    throw new Error(`Match: No nebula videos found for ${channel_slug}`);
+  let nebula_videos: NebulaVideos[] = [];
+  // Get specific nebula video/videos if passed in
+  if (rematch_nebula_slug && !rematch_all) {
+    const specificNebVideos = await NebulaVideos.find({
+      slug: { $in: rematch_nebula_slug },
+    });
+    if (specificNebVideos && specificNebVideos.length > 0) {
+      nebula_videos.push(...specificNebVideos);
+    } else {
+      throw new Error(
+        `Match: Nebula video ${rematch_nebula_slug} not found in DB`
+      );
+    }
   }
+
+  // Get all nebula videos if no specific video was passed in
+  if (!rematch_nebula_slug || rematch_all) {
+    nebula_videos = await NebulaVideos.find({
+      _id: {
+        $in: creator.nebula_videos?.map((video: any) => {
+          return video._id;
+        }),
+      },
+    }).select("title");
+    if (!nebula_videos) {
+      throw new Error(`Match: No nebula videos found for ${channel_slug}`);
+    }
+  }
+  // Match videos
 };

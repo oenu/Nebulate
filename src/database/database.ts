dfbeba1c5c550ca0cbc1d57ab77bbe84bf822097
oenu <51684443+oenu@users.noreply.github.com>
2022-06-24 13:43:54 -0400

    added database constructor

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,9 +1,11 @@
 // Gathers data from the database and creates a copy for distribution to the clients
 
-// const fs = require("fs");
-// const path = require("path");
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
 
 import logger from "../config/logger";
+
 import { NebulaVideo } from "../models/nebulaVideo";
 import { YoutubeVideo } from "../models/youtubeVideo";
 
@@ -44,13 +46,36 @@ const generateDatabase = async (): Promise<extensionDatabase> => {
       });
     }
   }
-  logger.verbose(videoPairs);
-  return {
-    videoPairs,
-    unmatchedVideos: [],
-    hash: "",
+
+  const unmatchedVideos = await YoutubeVideo.find({
+    matched: false,
+    youtube_video_id: { $exists: true },
+  }).select("youtube_video_id");
+
+  const database_prototype = {
+    videoPairs: videoPairs,
+    unmatchedVideos: unmatchedVideos.map((video) => video.youtube_video_id),
     generatedAt: new Date(),
   };
+
+  // Generate Cryptographic Hash
+  const privatePem = fs.readFileSync(path.join(__dirname, "/key.pem"));
+  const key = privatePem.toString();
+  const sign = crypto.createSign("RSA-SHA256");
+  sign.update(JSON.stringify(database_prototype));
+  const sig = sign.sign(key, "hex");
+
+  const database: extensionDatabase = {
+    ...database_prototype,
+    hash: sig,
+  };
+  await fs.promises.writeFile(
+    path.join(__dirname, "/simple_key.json"),
+    JSON.stringify(database),
+    "utf-8"
+  );
+  logger.verbose(database);
+  return database;
 };
 
 export default generateDatabase;

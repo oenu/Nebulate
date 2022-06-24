2e8ba28e52e42bccfc9cc56657f3719a907c70ab
oenu <51684443+oenu@users.noreply.github.com>
2022-06-24 11:40:07 -0400

    init

---
+++ src/database/database.ts
@@ -0,0 +1,6 @@
+// Gathers data from the database and creates a copy for distribution to the clients
+
+const fs = require("fs");
+const path = require("path");
+
+const generateDatabase = (): void => {};

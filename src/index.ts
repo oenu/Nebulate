a867aafa667dcba47659080eb7a78d13d5ace991
oenu <51684443+oenu@users.noreply.github.com>
2022-06-25 13:29:01 -0400

    imp - routing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express, { Request } from "express";
+import express from "express";
 const app = express();
-import type { Response } from "express";
+
 import "dotenv/config";
 
 // Constants
@@ -17,184 +17,36 @@ declare global {
 // Mongoose
 import mongoose from "mongoose";
 import { connectDB } from "./config/dbConfig";
-import { NebulaVideo } from "./models/nebulaVideo";
 connectDB();
 
 // Middleware
 import logger from "./config/logger";
 import auth from "./middleware/auth";
 import globalInit from "./store/store";
-import videosFromNebula from "./Functions/videosFromNebula";
-import registerCreatorInDB from "./Functions/registerCreatorInDB";
-import videosFromYoutube from "./Functions/videosFromYoutube";
-import matchVideos from "./Functions/matchVideos";
-import generateDatabase from "./database/database";
-import generateLookupTable from "./lookup_table/lookupTable";
+
 app.use(auth);
 
 // Routes
-app.get("/", (_req, res: Response) => {
-  res.send("Hello World!");
-  logger.info("Hello World!");
-});
+const generateDatabase = require("./routes/generateDatabase");
+app.use("/generate/database", generateDatabase);
 
-app.get("/generateDB", async (_req, res: Response) => {
-  await generateDatabase()
-    .then((database) => {
-      res.send(database);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
+const generateLookupTable = require("./routes/generateTable");
+app.use("/generate/table", generateLookupTable);
 
-app.get("/generateTable", async (_req, res: Response) => {
-  await generateLookupTable()
-    .then((lookupTable) => {
-      res.send(lookupTable);
-    })
-    .catch((err) => {
-      res.status(500).send(err);
-    });
-});
+const scrapeNebula = require("./routes/scrapeNebula");
+app.use("/scrape/nebula", scrapeNebula);
 
-app.get(
-  "/nebula/:channel_slug/:onlySearchNew?/:searchLimit?",
-  async (req: Request, res: Response) => {
-    const { channel_slug } = req.params;
-    const onlySearchNew = req.params.onlySearchNew
-      ? req.params.onlySearchNew === "true"
-      : true;
-    const searchLimit = req.params.searchLimit
-      ? parseInt(req.params.searchLimit)
-      : 10;
+const scrapeYoutube = require("./routes/scrapeYoutube");
+app.use("/scrape/youtube", scrapeYoutube);
 
-    if (!channel_slug) {
-      res.send("No channel_slug provided");
-      logger.error("No channel_slug provided");
-      return;
-    }
+const registerChannel = require("./routes/registerChannel");
+app.use("/register", registerChannel);
 
-    logger.info(`Updating ${channel_slug}`);
+const matchVideos = require("./routes/matchVideos");
+app.use("/match", matchVideos);
 
-    try {
-      await videosFromNebula(channel_slug, onlySearchNew, searchLimit);
-    } catch (error) {
-      logger.error(error);
-    }
-    res.send(`Updating ${channel_slug}`);
-  }
-);
-
-app.get("/register/:channel_slug", async (req: Request, res: Response) => {
-  // TODO: Change to post
-  const { channel_slug } = req.params;
-
-  if (!channel_slug) {
-    res.send("No channel_slug provided");
-    logger.error("No channel_slug provided");
-    return;
-  }
-
-  try {
-    await registerCreatorInDB(channel_slug);
-    res.send(`Registered ${channel_slug}`);
-  } catch (error: any) {
-    logger.error(error.message);
-
-    if (error.message) {
-      res.send(error.message);
-    } else {
-      res.send(error);
-    }
-  }
-});
-
-app.get(
-  "/youtube/:channel_slug/:onlySearchNew?/:videoScrapeLimit?",
-  async (req: Request, res: Response) => {
-    const channel_slug = req.params.channel_slug;
-    const onlySearchNew = req.params.onlySearchNew
-      ? req.params.onlySearchNew === "true"
-      : true;
-    const videoScrapeLimit = req.params.videoScrapeLimit
-      ? parseInt(req.params.videoScrapeLimit)
-      : 10;
-
-    if (!channel_slug) {
-      res.send("No channel_slug provided");
-      logger.error("No channel_slug provided");
-      return;
-    }
-
-    try {
-      await videosFromYoutube(channel_slug, onlySearchNew, videoScrapeLimit);
-      res.send(`Scraped ${channel_slug}`);
-    } catch (error: any) {
-      logger.error(error.message);
-
-      if (error.message) {
-        res.send(error.message);
-      } else {
-        res.send(error);
-      }
-    }
-  }
-);
-
-app.get(
-  "/match/:channel_slug/:rematch_nebula_slug?/:rematch_yt_id?",
-  async (req: Request, res: Response) => {
-    const channel_slug = req.params.channel_slug;
-
-    // HACK: #40
-    const rematch_nebula_slug = req.params?.rematch_nebula_slug
-      ? [req.params.rematch_nebula_slug]
-      : undefined;
-    const rematch_yt_id = req.params?.rematch_yt_id
-      ? [req.params.rematch_yt_id]
-      : undefined;
-
-    // TODO: #39 Change to post and change to body parser
-    if (!channel_slug) {
-      res.send("No channel_slug provided");
-      logger.error("No channel_slug provided");
-      return;
-    }
-    try {
-      await matchVideos(
-        channel_slug,
-
-        rematch_nebula_slug,
-        rematch_yt_id
-      );
-
-      res.send(`Matched ${channel_slug}`);
-    } catch (error: any) {
-      logger.error(error.message);
-      throw error;
-    }
-  }
-);
-
-app.get("/database", async (_req: Request, res: Response) => {
-  await generateDatabase();
-  res.send("Generated database");
-});
-
-app.get("/lookup/:youtube_video_id", async (_req: Request, res: Response) => {
-  const youtube_video_id = _req.params.youtube_video_id;
-  if (!youtube_video_id) {
-    res.send("No youtube_video_id provided");
-  } else {
-    const video = await NebulaVideo.findOne({
-      youtube_video_id: youtube_video_id,
-    }).lean();
-    if (video) {
-      res.send(video.slug);
-    }
-  }
-});
+const lookupRequest = require("./routes/lookupRequest");
+app.use("/lookup", lookupRequest);
 
 // Start the server
 mongoose.connection.once("open", async () => {

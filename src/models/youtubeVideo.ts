81843b909b1266f0862f43ac11300da753af3b66
oenu <51684443+oenu@users.noreply.github.com>
2022-06-25 09:13:40 -0400

    removed youtube video methods

--- src/models/youtubeVideo.ts
+++ src/models/youtubeVideo.ts
@@ -86,23 +86,6 @@ const youtubeVideoSchema = new Schema<YoutubeVideoDocument>(
       type: "String",
       index: true,
     },
-    nebula_video_object_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "NebulaVideo",
-    },
-    nebula_video_slug: {
-      // "real-lawyer-reacts-to-will-smith-slapping-chris-rock"
-      type: "String",
-      index: true,
-    },
-    matched: {
-      type: "Boolean",
-      index: true,
-      default: false,
-    },
-    match_strength: {
-      type: "Number",
-    },
     creator_object_id: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Creator",
@@ -115,47 +98,47 @@ const youtubeVideoSchema = new Schema<YoutubeVideoDocument>(
 );
 
 // Methods
-youtubeVideoSchema.methods.setMatch = async function (
-  nebulaVideo: NebulaVideoType,
-  matchStrength: number
-): Promise<void> {
-  this.matched = true;
-  this.nebula_video_object_id = nebulaVideo._id;
-  this.nebula_video_slug = nebulaVideo.slug;
-  this.match_strength = matchStrength;
-  await this.save();
-};
+// youtubeVideoSchema.methods.setMatch = async function (
+//   nebulaVideo: NebulaVideoType,
+//   matchStrength: number
+// ): Promise<void> {
+//   this.matched = true;
+//   this.nebula_video_object_id = nebulaVideo._id;
+//   this.nebula_video_slug = nebulaVideo.slug;
+//   this.match_strength = matchStrength;
+//   await this.save();
+// };
 
-youtubeVideoSchema.methods.updateMatch = async function (
-  nebulaVideo: NebulaVideoType,
-  matchStrength: number
-): Promise<void> {
-  if (this.nebula_video_object_id !== nebulaVideo._id) {
-    const oldNebulaVideo = await NebulaVideo.findById(
-      this.nebula.video_object_id
-    );
-    if (oldNebulaVideo) {
-      if (oldNebulaVideo.match_strength) {
-        if (matchStrength > oldNebulaVideo.match_strength) return;
-        await oldNebulaVideo.removeMatch(nebulaVideo);
-      }
-    }
-    await this.setMatch(nebulaVideo, matchStrength);
-  }
-};
+// youtubeVideoSchema.methods.updateMatch = async function (
+//   nebulaVideo: NebulaVideoType,
+//   matchStrength: number
+// ): Promise<void> {
+//   if (this.nebula_video_object_id !== nebulaVideo._id) {
+//     const oldNebulaVideo = await NebulaVideo.findById(
+//       this.nebula.video_object_id
+//     );
+//     if (oldNebulaVideo) {
+//       if (oldNebulaVideo.match_strength) {
+//         if (matchStrength > oldNebulaVideo.match_strength) return;
+//         await oldNebulaVideo.removeMatch(nebulaVideo);
+//       }
+//     }
+//     await this.setMatch(nebulaVideo, matchStrength);
+//   }
+// };
 
-youtubeVideoSchema.methods.removeMatch = async function (
-  replacementVideo?: YoutubeVideoType
-): Promise<void> {
-  logger.warn(
-    `Removing match for ${this.title}, replacing with ${replacementVideo?.title}`
-  );
-  this.matched = false;
-  this.nebula_video_object_id = null;
-  this.nebula_video_slug = null;
-  this.match_strength = null;
-  await this.save();
-};
+// youtubeVideoSchema.methods.removeMatch = async function (
+//   replacementVideo?: YoutubeVideoType
+// ): Promise<void> {
+//   logger.warn(
+//     `Removing match for ${this.title}, replacing with ${replacementVideo?.title}`
+//   );
+//   this.matched = false;
+//   this.nebula_video_object_id = null;
+//   this.nebula_video_slug = null;
+//   this.match_strength = null;
+//   await this.save();
+// };
 
 youtubeVideoSchema.statics.findByYoutubeVideoId = async function (
   youtubeVideoId: string

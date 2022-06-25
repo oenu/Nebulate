5a65f4dce9c3d5882181275eac0af06d69601478
oenu <51684443+oenu@users.noreply.github.com>
2022-06-25 14:20:41 -0400

    added asset importing

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -29,6 +29,10 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   resolve: {

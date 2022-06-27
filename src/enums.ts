98291b8e55817989c48ceb15ace1fd9a474377eb
oenu <51684443+oenu@users.noreply.github.com>
2022-06-27 09:42:00 -0400

    added css enums

--- src/enums.ts
+++ src/enums.ts
@@ -7,3 +7,13 @@ export enum Messages {
 export enum Alarms {
   UPDATE_LOOKUP_TABLE = "UPDATE_LOOKUP_TABLE",
 }
+
+export enum CSS {
+  NEBULA = "NEBULA",
+  CREATOR = "CREATOR",
+}
+
+export enum CSS_CLASSES {
+  NEBULA = "nebulate-video-css",
+  CREATOR = "nebulate-creator-css",
+}

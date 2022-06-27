85979386054b3471364af32748a09e232fb11d0b
oenu <51684443+oenu@users.noreply.github.com>
2022-06-27 12:44:54 -0400

    added addNebulaControls v0.1

--- src/functions/domMethods.ts
+++ src/functions/domMethods.ts
@@ -1,5 +1,7 @@
 import { redirectHandler } from "../content_script";
-import { CSS, CSS_CLASSES } from "../enums";
+import { CSS, CSS_CLASSES, Messages } from "../enums";
+
+// const cssFile = require("../css/video.css");
 
 export const loadCSS = (css: CSS) => {
   if (css === CSS.NEBULA_VIDEO) {
@@ -7,6 +9,8 @@ export const loadCSS = (css: CSS) => {
     const style = document.createElement("style");
     style.className = CSS_CLASSES.NEBULA;
     style.textContent = generateNebulaStyling();
+    // style.textContent = cssFile.toString();
+    // console.log(cssFile.toString());
     console.debug("loadCSS: loading styling" + css);
     setTimeout(() => {
       head.appendChild(style);
@@ -15,6 +19,7 @@ export const loadCSS = (css: CSS) => {
   if (css === CSS.CREATOR) {
     const head = document.head || document.getElementsByTagName("head")[0];
     const style = document.createElement("style");
+
     style.className = CSS_CLASSES.CREATOR;
     style.textContent = generateCreatorStyling();
     console.debug("loadCSS: loading styling" + css);
@@ -49,49 +54,18 @@ export const unloadCSS = (css: CSS) => {
   }
 };
 
-// export const setBg = (css: string) => {
-//   unloadCSS( );
-//   setTimeout(() => loadCSS(css));
-// };
 // IDEA: #11 Animated background slow transition through nebula colors, options menu
 export const generateNebulaStyling = () => {
-  // Default case for video player for glow effect
-  // transition: box-shadow 0.5s ease-in-out 2s;
   let default_css = `#player {
-    transition: box-shadow 0.6s cubic-bezier(0.165, 0.84, 0.44, 1); \n
-            box-shadow: -10px 0 40px rgb(62,	187,	243), 10px 0 40px rgb(88,	80,	209	);
-            border-radius: 10px;
-          };
+  transition: box-shadow 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  box-shadow: -10px 0 40px rgb(62, 187, 243), 10px 0 40px rgb(88, 80, 209);
+}
 
-  #player-theater-container {
-  transition: box-shadow 0.5s ease-in-out 2s;
-    
-      box-shadow: \n
-box-shadow: -10px 0 40px rgb(62,	187,	243), 10px 0 40px rgb(88,	80,	209	);
-
-
-
-
-      0 0 50px #fff, \n
-      -10px 0 80px #f0f, \n
-      10px 0 80px #0ff; \n
-  };\n
-  `;
-
-  // Select Internal video player to get tight side styling
-  let shorts_css = `#movie_player > div.html5-video-container > video {
-    transition: box-shadow 0.5s ease-in-out 2s;
-  transition: all 0.6s cubic-bezier(0.165, 0.84, 0.44, 1); \n     
-    box-shadow: \n
-      inset 0 0 60px whitesmoke, \n
-      inset 20px 0 80px #f0f, \n
-      inset -20px 0 80px #0ff, \n
-      inset 20px 0 300px #f0f, \n
-      inset -20px 0 300px #0ff, \n
-      0 0 50px #fff, \n
-      -10px 0 80px #f0f, \n
-      10px 0 80px #0ff; \n
-  }\n`;
+#movie_player > div.html5-video-container > video {
+  transition: box-shadow 0.6s cubic-bezier(0.165, 0.84, 0.44, 1);
+  box-shadow: -10px 0 10vw rgb(62, 187, 243), 10px 0 10vw rgb(88, 80, 209);
+  clip-path: inset(0px -100vw 0px -100vw);
+}`;
 
   // Check aspect ratio of video
   const player = document.querySelector(
@@ -103,7 +77,7 @@ box-shadow: -10px 0 40px rgb(62,	187,	243), 10px 0 40px rgb(88,	80,	209	);
   if (player_height > player_width)
     console.info("generateNebulaStyling: Vertical video detected");
 
-  return player_height > player_width ? shorts_css : default_css;
+  return player_height > player_width ? default_css : default_css;
 };
 
 // IDEA: #10 Add css for creator home channel to identify as nebula creator
@@ -118,23 +92,59 @@ export const generateCreatorStyling = () => {
 // let youtube_left_controls: Element | null = null;
 let youtube_right_controls: Element | null = null;
 
-export const addNebulaControls = () => {
-  // Add nebula controls
-  const nebulate_button_exists = document.getElementById("nebulate-btn");
+export const addCreatorButton = (creator_slug: string) => {
+  // const creator_button = document.createElement("button");
+  // creator_button.className = "nebulate-creator-button";
+  // creator_button.innerHTML = "Nebulate Creator";
+  // creator_button.addEventListener("click", () => {
+  //   redirectHandler(Messages.CREATOR_REDIRECT);
+  // });
+  const nebulate_button_exists = document.getElementById("nebulate-video-btn");
   if (!nebulate_button_exists) {
     const nebulate_button = document.createElement("img");
     nebulate_button.src = chrome.runtime.getURL("assets/nebula_temp_light.png");
 
     // Assign DOM element attributes
-    nebulate_button.className = "ytp-button " + "nebulate-btn";
-    nebulate_button.id = "nebulate-btn";
+    nebulate_button.className = "ytp-button " + "nebulate-video-btn";
+    nebulate_button.id = "nebulate-video-btn";
     nebulate_button.title = "RIGHT View this video on Nebula";
 
     youtube_right_controls =
       document.getElementsByClassName("ytp-right-controls")[0];
 
     youtube_right_controls.prepend(nebulate_button);
-    nebulate_button.addEventListener("click", redirectHandler);
+    nebulate_button.addEventListener("click", () => {
+      redirectHandler(Messages.CREATOR_REDIRECT);
+    });
+    return;
+  } else {
+    console.debug("addNebulaControls: Nebulate button already exists");
+    return;
+  }
+};
+
+export const addNebulaControls = () => {
+  // Add nebula controls
+  const nebulate_button_exists = document.getElementById(
+    "nebulate-creator-btn"
+  );
+  if (!nebulate_button_exists) {
+    const creator_button = document.createElement("img");
+    creator_button.src = chrome.runtime.getURL("assets/nebula_temp_light.png");
+
+    // Assign DOM element attributes
+    creator_button.className = "ytp-button " + "nebulate-creator-btn";
+    creator_button.id = "nebulate-creator-btn";
+    creator_button.title = "RIGHT View this creator on Nebula";
+
+    youtube_right_controls =
+      document.getElementsByClassName("ytp-right-controls")[0];
+
+    document.querySelector("#sponsor-button")?.prepend(creator_button);
+
+    creator_button.addEventListener("click", () => {
+      redirectHandler(Messages.CREATOR_REDIRECT);
+    });
     return;
   } else {
     console.debug("addNebulaControls: Nebulate button already exists");
@@ -144,7 +154,7 @@ export const addNebulaControls = () => {
 
 export const removeNebulaControls = () => {
   console.debug("removeNebulaControls");
-  const nebulate_button = document.getElementById("nebulate-btn");
+  const nebulate_button = document.getElementById("nebulate-video-btn");
   if (nebulate_button) {
     console.log("removeNebulaControls: Nebulate button exists");
     nebulate_button.remove();

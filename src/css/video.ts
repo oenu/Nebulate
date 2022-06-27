c462ac4838572a11a73e14e019f4b2f3fd3fd729
oenu <51684443+oenu@users.noreply.github.com>
2022-06-27 06:51:59 -0400

    init ts styling file

---
+++ src/css/video.ts
@@ -0,0 +1,9 @@
+import type * as CSS from "csstype";
+
+const nebula_video_css: CSS.Properties = {
+  transform: "translateY(0)",
+  boxShadow:
+    "inset 0 0 60px whitesmoke, inset 20px 0 80px #f0f,\n    inset -20px 0 80px #0ff, inset 20px 0 300px #f0f, inset -20px 0 300px #0ff,\n    0 0 50px #fff, -10px 0 80px #f0f, 10px 0 80px #0ff",
+};
+
+export { nebula_video_css };

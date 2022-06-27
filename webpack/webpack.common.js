9f895f05c3055b0d5a8832cc3481860999b0a6ec
oenu <51684443+oenu@users.noreply.github.com>
2022-06-27 12:43:26 -0400

    added style loaders webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -33,6 +33,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   resolve: {

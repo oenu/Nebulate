8f93c1e9c6aa8f9cc86994693a9382dfaa945d11
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 09:44:13 -0400

    removed excess files

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,6 @@
 // jest.config.ts
 import type { InitialOptionsTsJest } from "ts-jest";
-import { defaults as tsjPreset } from "ts-jest/presets";
+// import { defaults as tsjPreset } from "ts-jest/presets";
 // import { defaultsESM as tsjPreset } from 'ts-jest/presets'
 // import { jsWithTs as tsjPreset } from 'ts-jest/presets'
 // import { jsWithTsESM as tsjPreset } from 'ts-jest/presets'
@@ -9,13 +9,13 @@ import { defaults as tsjPreset } from "ts-jest/presets";
 
 const config: InitialOptionsTsJest = {
   silent: false,
-  setupFiles: ["dotenv/config"],
-  preset: "@shelf/jest-mongodb",
+  // setupFiles: ["dotenv/config"],
+  // preset: "@shelf/jest-mongodb",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  transform: {
-    ...tsjPreset.transform,
-  },
+  // transform: {
+  //   ...tsjPreset.transform,
+  // },
 };
 
 export default config;

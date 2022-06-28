38cfe09e6330d58f5c76448b957a7e6520065622
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 00:14:06 -0400

    init jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,25 @@
 module.exports = {
-  preset: "ts-jest",
+  // preset: "ts-jest",
+
   testEnvironment: "node",
 };
+
+// jest.config.ts
+import type { InitialOptionsTsJest } from "ts-jest";
+import { defaults as tsjPreset } from "ts-jest/presets";
+// import { defaultsESM as tsjPreset } from 'ts-jest/presets'
+// import { jsWithTs as tsjPreset } from 'ts-jest/presets'
+// import { jsWithTsESM as tsjPreset } from 'ts-jest/presets'
+// import { jsWithBabel as tsjPreset } from 'ts-jest/presets'
+// import { jsWithBabelESM as tsjPreset } from 'ts-jest/presets'
+
+const config: InitialOptionsTsJest = {
+  // [...]
+  setupFiles: ["dotenv/config"],
+  preset: "@shelf/jest-mongodb",
+  transform: {
+    ...tsjPreset.transform,
+  },
+};
+
+export default config;

2777c2c717720261aaeef5d026538f6b7477c229
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 08:48:27 -0400

    init database tests

--- src/__tests__/registerCreatorInDB.test.ts
+++ src/__tests__/registerCreatorInDB.test.ts
@@ -1,40 +1,19 @@
-// const mongoose = require('mongoose');
+import mongoose from "mongoose";
+// import axios from "axios";
+// jest.mock("axios");
+import { Creator } from "../models/creator";
+import { NebulaVideo } from "../models/nebulaVideo";
+import { YoutubeVideo } from "../models/youtubeVideo";
+// import { creatorFromNebula } from "../Functions/registerCreatorInDB";
 require("dotenv").config("./.env");
 
+// Mocks
+// const creatorFromNebulaMock = require("../__mocks__/creatorFromNebula");
+
 //https://github.com/shelfio/jest-mongodb
+//https://vhudyma-blog.eu/3-ways-to-mock-axios-in-jest/
 
-// beforeAll(async () => {
-//   await mongoose.connect(process.env.DATABASE_TESTING_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//   });
-// }
-
-// import registerCreatorInDB from "../Functions/registerCreatorInDB";
-// import creator
-
-// describe("registerCreatorInDB", () => {
-//   it("should be a function", () => {
-//     expect(registerCreatorInDB).toBeInstanceOf(Function);
-//   });
-
-//   it ("should throw an error if creator already exists in DB", async () => {
-//     const channel_slug = "test";
-//     const creator = new Creator({ slug: channel_slug });
-//     await creator.save();
-//     expect(() => registerCreatorInDB(channel_slug)).toThrow(Error);
-//   }
-// });
-
-import { MongoClient } from "mongodb";
-import registerCreatorInDB from "../Functions/registerCreatorInDB";
-import Creator from "../models/creator";
-
-describe("insert", () => {
-  let connection: any;
-  let db: any;
-
+describe("database tests", () => {
   it("should be able to find env variables", () => {
     expect(process.env.DATABASE_URI).toBeDefined();
     expect(process.env.DATABASE_TESTING_URI).toBeDefined();
@@ -44,20 +23,88 @@ describe("insert", () => {
     if (!process.env.DATABASE_TESTING_URI) {
       throw new Error("DATABASE_TESTING_URI not set");
     }
-    connection = await MongoClient.connect(process.env.DATABASE_TESTING_URI);
-    db = await connection.db();
+    await mongoose.connect(process.env.DATABASE_TESTING_URI);
+    console.log("connected to mongoose");
   });
 
   afterAll(async () => {
-    await connection.close();
+    await mongoose.connection.close();
   });
 
-  it("should insert a creator into collection", async () => {
-    const users = db.collection("users");
+  describe("Creator", () => {
+    it("should be able to add a creator to the database", async () => {
+      const creator = new Creator({
+        nebula_id: "test_creator_nebula_id",
+        slug: "test_creator_slug",
+        title: "test_creator_title",
+        description: "test_creator_description",
+        type: "test_creator_type",
+        zype_id: "test_creator_zype_id",
+        youtube_id: "test_creator_youtube_id",
+        youtube_upload_id: "test_creator_youtube_upload_id",
+      });
 
-    const creator = registerCreatorInDB("test");
+      const response = await Creator.create(creator);
+      expect(response?._id).toBeDefined();
+    });
 
-    // const insertedUser = await users.findOne({ _id: "some-user-id" });
-    // expect(insertedUser).toEqual(mockUser);
+    it("should be able to find a creator in the database", async () => {
+      const testCreator = await Creator.findOne({ slug: "test_slug" });
+      expect(testCreator?.slug).toBe("test_slug");
+    });
+  });
+
+  describe("NebulaVideo", () => {
+    it("should be able to add a video to the database", async () => {
+      const video = new NebulaVideo({
+        nebula_video_id: "test_nebula_video_id",
+        slug: "test_nebula_video_slug",
+        title: "test_nebula_video_title",
+        short_description: "test_nebula_video_short_description",
+        duration: 1000,
+        published_at: new Date(),
+        channel_id: "test_creator_nebula_id",
+        channel_slug: "test_creator_nebula_slug",
+        channel_slugs: ["test_creator_nebula_slug"],
+        channel_title: "test_creator_nebula_title",
+        share_url: "test_nebula_video_share_url",
+        matched: false,
+      });
+
+      const response = await NebulaVideo.create(video);
+      expect(response?._id).toBeDefined();
+    });
+
+    it("should be able to find a video in the database", async () => {
+      const testVideo = await NebulaVideo.findOne({
+        slug: "test_nebula_video_slug",
+      });
+      expect(testVideo?.slug).toBe("test_nebula_video_slug");
+    });
+  });
+
+  describe("YoutubeVideo", () => {
+    it("should be able to add a video to the database", async () => {
+      const video = new YoutubeVideo({
+        youtube_video_id: "test_youtube_video_id",
+        published_at: new Date(),
+        playlist_id: "test_playlist_id",
+        title: "test_youtube_video_title",
+        channel_id: "test_creator_youtube_channel_id",
+        channel_slug: "test_creator_slug",
+        channel_slugs: ["test_creator_slug"],
+        channel_title: "test_creator_youtube_title",
+      });
+
+      const response = await YoutubeVideo.create(video);
+      expect(response?._id).toBeDefined();
+    });
+
+    it("should be able to find a video in the database", async () => {
+      const testVideo = await YoutubeVideo.findOne({
+        youtube_video_id: "test_youtube_video_id",
+      });
+      expect(testVideo?.youtube_video_id).toBe("test_youtube_video_id");
+    });
   });
 });

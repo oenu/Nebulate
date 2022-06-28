0707602280bd449ff2c3499fc7048a5a86c6358e
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 16:46:29 -0400

    cron init

---
+++ src/cron/updateCreators.ts
@@ -0,0 +1,37 @@
+import { Creator } from "../models/creator";
+
+const hourScrapeInterval = 6;
+
+const updateCreatorsCron = async () => {
+  // Get all creators
+  const creators = await Creator.find({}).select(
+    "last_scraped_nebula last_scraped_youtube last_matched slug"
+  );
+
+  const scrapeThreshold =
+    new Date().getTime() - hourScrapeInterval * 60 * 60 * 1000;
+
+  // For each creator, check if it needs to be updated
+  for (const creator of creators) {
+    let needsRematch = false;
+    // Check if the creator needs to be updated
+    if (creator.last_scraped_nebula.getTime() < scrapeThreshold) {
+      // Scrape the creator's videos from Nebula
+      await creator.scrapeNebula();
+      needsRematch = true;
+    }
+    if (creator.last_scraped_youtube.getTime() < scrapeThreshold) {
+      // Scrape the creator's videos from Youtube
+      await creator.scrapeYoutube();
+      needsRematch = true;
+    }
+
+    if (creator.last_matched.getTime() < scrapeThreshold * 3 || needsRematch) {
+      // Match the creator's videos
+      // Run less often due to the time it takes to match
+      await creator.matchVideos();
+    }
+  }
+};
+
+export default updateCreatorsCron;

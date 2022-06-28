262c3e66c7434d498cba63039738b69d8d0846bf
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 15:22:15 -0400

    changed table type

--- src/functions/downloadTable.ts
+++ src/functions/downloadTable.ts
@@ -1,11 +1,26 @@
 import { server_url } from "../background";
 
 const downloadTable = async (): Promise<any> => {
+  let response;
   try {
     console.log("background.js: downloading lookup table");
-    const response = await fetch(`${server_url}/api/table`);
-    const data = await response.json();
-    return data;
+
+    // Check for existing table
+    const table = await chrome.storage.local.get("lookupTable");
+    if (!table) {
+      response = await fetch(`${server_url}/api/table`);
+    } else {
+      response = await fetch(`${server_url}/api/table/${table.lookupTable.id}`);
+    }
+
+    // Check response
+    if (response.status === 204) {
+      console.log("background.js: table is up to date");
+      return;
+    } else if (response.status === 200) {
+      const data = await response.json();
+      return data;
+    }
   } catch (error) {
     console.log(error);
     return;

fdee948b0482f3a6a81645071c84efccf9ab3e5d
oenu <51684443+oenu@users.noreply.github.com>
2022-06-27 21:23:24 -0400

    added clear message enum

--- src/enums.ts
+++ src/enums.ts
@@ -1,8 +1,9 @@
 export enum Messages {
-  NEW_VIDEO = "NEW_VIDEO",
-  NO_SLUG_REDIRECT = "NO_SLUG_REDIRECT",
-  NEBULA_REDIRECT = "NEBULA_REDIRECT",
-  CREATOR_REDIRECT = "CREATOR_REDIRECT",
+  NEW_VIDEO = "NEW_VIDEO", // Sent to the content script when a new video is loaded.
+  CLEAR = "CLEAR", // Sent to the content script when the page is no longer a video.
+  NO_SLUG_REDIRECT = "NO_SLUG_REDIRECT", // Sent to the content script when a request is made for a video that has no slug.
+  NEBULA_REDIRECT = "NEBULA_REDIRECT", // Sent to the background script to request a redirect to a nebula video.
+  CREATOR_REDIRECT = "CREATOR_REDIRECT", // Sent to the background script to request a redirect to a creator.
 }
 
 export enum Alarms {

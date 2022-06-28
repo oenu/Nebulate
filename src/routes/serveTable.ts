54941c3ea5da354d158ac26bd5c1fc9dde339893
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 05:15:01 -0400

    removed log

--- src/routes/serveTable.ts
+++ src/routes/serveTable.ts
@@ -6,7 +6,6 @@ import serveLookupTable from "../lookup_table/lookupTable";
 app.get("/:table_version?", async (req: Request, res: Response) => {
   console.time("serveLookupTable");
   const version = req.params.table_version;
-  console.log("Responding to req for " + version);
   try {
     const response = await serveLookupTable(undefined, version);
     if (response === true) {

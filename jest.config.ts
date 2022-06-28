f76debdd35d500841a9659edfe264e38c37627ab
oenu <51684443+oenu@users.noreply.github.com>
2022-06-28 10:29:29 -0400

    cleaned up jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,10 @@
-// jest.config.ts
-import type { InitialOptionsTsJest } from "ts-jest";
-// import { defaults as tsjPreset } from "ts-jest/presets";
-// import { defaultsESM as tsjPreset } from 'ts-jest/presets'
-// import { jsWithTs as tsjPreset } from 'ts-jest/presets'
-// import { jsWithTsESM as tsjPreset } from 'ts-jest/presets'
-// import { jsWithBabel as tsjPreset } from 'ts-jest/presets'
-// import { jsWithBabelESM as tsjPreset } from 'ts-jest/presets'
-
-const config: InitialOptionsTsJest = {
+const config = {
+  preset: "ts-jest",
+  globalSetup: "<rootDir>/test/globalSetup.ts",
+  globalTeardown: "<rootDir>/test/globalTeardown.ts",
+  setupFilesAfterEnv: ["<rootDir>/test/setupFile.ts"],
   silent: false,
-  // setupFiles: ["dotenv/config"],
-  // preset: "@shelf/jest-mongodb",
-  testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  // transform: {
-  //   ...tsjPreset.transform,
-  // },
 };
 
 export default config;

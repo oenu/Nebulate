9f5ad1ad778a9de557251dee3b59a3312c79774a
oenu <51684443+oenu@users.noreply.github.com>
2022-06-30 04:56:37 -0400

    docs and onlylocal

--- src/index.ts
+++ src/index.ts
@@ -22,37 +22,46 @@ connectDB();
 import logger from "./utils/logger";
 import auth from "./middleware/auth";
 import globalInit from "./auth/store";
+import { onlyLocal } from "./middleware/onlyLocal";
 
 app.use(auth);
 
 // Routes
-
+// Respond to request for latest version of the lookup table
 const serveLookupTable = require("./routes/serveTable");
 app.use("/api/table", serveLookupTable);
 
+// Trigger a scrape of nebula videos for specific creator
 const scrapeNebula = require("./routes/scrapeNebula");
-app.use("/scrape/nebula", scrapeNebula);
+app.use("/scrape/nebula", onlyLocal, scrapeNebula);
 
+// Trigger a scrape of youtube videos for specific creator
 const scrapeYoutube = require("./routes/scrapeYoutube");
-app.use("/scrape/youtube", scrapeYoutube);
+app.use("/scrape/youtube", onlyLocal, scrapeYoutube);
 
+// Register a new creator
 const registerChannel = require("./routes/registerChannel");
-app.use("/register", registerChannel);
+app.use("/register", onlyLocal, registerChannel);
 
+// Match videos from Nebula and Youtube for specific creator
 const matchVideos = require("./routes/matchVideos");
-app.use("/match", matchVideos);
+app.use("/match", onlyLocal, matchVideos);
 
+// Search internal mappings for a specific nebula video based on a provided youtube video id
 const lookupRequest = require("./routes/lookupRequest");
 app.use("/api/lookup", lookupRequest);
 
+// Trigger the registration of all creators that have manual youtube id mappings
 const registerAllCreators = require("./routes/registerAllCreators");
-app.use("/register_all_serious", registerAllCreators);
+app.use("/register_all_serious", onlyLocal, registerAllCreators);
 
+// Match all videos from Nebula and Youtube for all creators without scraping new videos
 const matchAllCreators = require("./routes/matchAllCreators");
-app.use("/match_all_serious", matchAllCreators);
+app.use("/match_all_serious", onlyLocal, matchAllCreators);
 
+// Trigger a scrape of all creators and rematching of all videos
 const updateAllCreators = require("./routes/updateAllCreators");
-app.use("/update_all_creators", updateAllCreators);
+app.use("/update_all_creators", onlyLocal, updateAllCreators);
 
 // Start the server
 mongoose.connection.once("open", async () => {

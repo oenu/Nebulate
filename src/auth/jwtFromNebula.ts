a1584f79d3404bd92f67641fffd0f8f12977c9c4
oenu <51684443+oenu@users.noreply.github.com>
2022-06-29 23:52:05 -0400

    reformatted function

--- src/auth/jwtFromNebula.ts
+++ src/auth/jwtFromNebula.ts
@@ -16,41 +16,43 @@ import keyFromNebula from "./keyFromNebula";
 export const jwtFromNebula = async (): Promise<string> => {
   try {
     if (global.key === undefined) {
+      // Fetch key from File
       try {
-        logger.info("Global key undefined, fetching from file");
+        logger.debug("jwtFromNebula: Global key undefined, fetching from file");
         const simple_key = await fs.promises.readFile(
           path.join(__dirname, "..", "store", "simple_key.txt"),
           "utf-8"
         );
         global.key = simple_key;
       } catch {
-        logger.error("No key found in store, fetching from Nebula");
-        try {
-          await keyFromNebula();
-          // global token should be set
-        } catch (error) {
-          throw error;
-        }
+        logger.error(
+          "jwtFromNebula: No key found in store, fetching from Nebula"
+        );
+        await keyFromNebula();
         if (global.key === undefined) {
-          throw new Error("Fallback method: keyFromNebula failed to set key");
+          throw new Error(
+            "jwtFromNebula: Fallback method: keyFromNebula failed to set key"
+          );
         }
       }
     }
 
+    // Fetch JWT from Nebula
     const url = "https://api.watchnebula.com/api/v1/authorization/";
     const response = await axios.post(url, {
       data: { Authorization: `Token ${global.key}` },
     });
 
+    // Write JWT to file
     await fs.promises.writeFile(
       path.join(__dirname, "..", "store", "json_token.txt"),
       response.data.token
     );
 
+    // Set global.token to JWT
     global.token = response.data.token;
     return response.data.token;
   } catch (error) {
-    logger.error(error);
     logger.error("JWT: Unable to get token from Nebula");
     throw new Error("JWT: Unable to get token from Nebula");
   }

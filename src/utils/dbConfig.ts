8d8289a1b13c4de28484fcf5ac1971935045353b
oenu <51684443+oenu@users.noreply.github.com>
2022-07-04 11:03:57 -0400

    dbconfig delay

--- src/utils/dbConfig.ts
+++ src/utils/dbConfig.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import logger from "./logger";
 // Fix undefined ENV typing
 declare let process: { env: { [key: string]: string } };
 
@@ -14,9 +14,14 @@ export const connectDB = async () => {
     console.log(process.env.NODE_ENV);
     // Connect to the database
     if (process.env.NODE_ENV === "dev") {
-      console.log("Connecting to mongoDB in dev mode");
+      logger.info("Connecting to mongoDB in dev mode");
+      logger.warn("WARNING: YOU SHOULD HAVE MONGODB RUNNING LOCALLY");
       await mongoose.connect("mongodb://localhost:27017/nebulate");
     } else if (process.env.DATABASE_URI) {
+      // Possibly first time, delay for a bit to allow mongodb to start
+      logger.info("Waiting for mongodb to start");
+      await new Promise((resolve) => setTimeout(resolve, 30000));
+      logger.info("Connecting to mongoDB in prod mode");
       await mongoose.connect(process.env.DATABASE_URI);
     } else {
       // This cannot be missing, HCF

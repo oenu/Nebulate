cacad18d10896cc9bd772ad70b28ba19ba360d32
oenu <51684443+oenu@users.noreply.github.com>
2022-07-04 04:14:23 -0400

    added try catch

--- src/routes/registerAllCreators.ts
+++ src/routes/registerAllCreators.ts
@@ -6,9 +6,14 @@ const app = express();
 import registerAllCreators from "../bulk_methods/registerAllCreators";
 
 app.put("/", async (_req: Request, res: Response) => {
-  logger.warn("registerAllCreators: Registering all creators");
-  registerAllCreators();
-  res.send("Registering all creators");
+  try {
+    logger.warn("registerAllCreators: Registering all creators");
+    registerAllCreators();
+    res.send("Registering all creators");
+  } catch (error) {
+    logger.error(error);
+    res.status(500).send(error);
+  }
 });
 
 module.exports = app;

0ba94aac48e7a0477f8e1af0c823ed77104873f8
oenu <51684443+oenu@users.noreply.github.com>
2022-07-05 12:31:37 -0400

    added reconnect

--- src/utils/dbConfig.ts
+++ src/utils/dbConfig.ts
@@ -12,20 +12,38 @@ declare let process: { env: { [key: string]: string } };
 export const connectDB = async () => {
   try {
     console.log(process.env.NODE_ENV);
-    // Connect to the database
+    // Connect to the database in development environment
     if (process.env.NODE_ENV === "dev") {
       logger.info("Connecting to mongoDB in dev mode");
       logger.warn("WARNING: YOU SHOULD HAVE MONGODB RUNNING LOCALLY");
-      await mongoose.connect("mongodb://localhost:27017/nebulate");
-    } else if (process.env.DATABASE_URI) {
-      // Possibly first time, delay for a bit to allow mongodb to start
-      logger.info("Waiting for mongodb to start");
-      await new Promise((resolve) => setTimeout(resolve, 30000));
-      logger.info("Connecting to mongoDB in prod mode");
-      await mongoose.connect(process.env.DATABASE_URI);
-    } else {
-      // This cannot be missing, HCF
-      throw new Error("DATABASE_URI is not defined");
+      await mongoose.connect("mongodb://localhost:27017");
+
+      // Connect to the database in production environment
+    } else if (process.env.NODE_ENV === "prod") {
+      if (process.env.DATABASE_URI === undefined) {
+        throw new Error("connectDB: Environment variables not set");
+      }
+      // Attempt to connect to the database
+      let i = 0;
+      while (i < 10) {
+        try {
+          logger.info("Connecting to mongoDB in prod mode");
+          await mongoose.connect(process.env.DATABASE_URI);
+          break;
+        } catch (error) {
+          i++;
+          logger.info(
+            "Couldn't connect to mongoDB, retrying in 15 seconds, attempt " +
+              i +
+              "/10"
+          );
+          await new Promise((resolve) => setTimeout(resolve, 15000));
+        }
+      }
+      // If the database connection fails 10 times, throw an error
+      if (i === 10) {
+        throw new Error("Couldn't connect to mongoDB after 10 attempts");
+      }
     }
   } catch (err) {
     console.error(err);

c522c5bc0d3cd2d26ba65b95d40a17189cb2e219
oenu <51684443+oenu@users.noreply.github.com>
2022-07-05 23:10:51 -0400

    filled temps

--- extension/src/options.tsx
+++ extension/src/options.tsx
@@ -1,1 +1,8 @@
 // Placeholder
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(<Options />, document.getElementById("root"));
+
+function Options() {
+  return <div>Please excuse the lack of options.</div>;
+}
--- extension/src/popup.tsx
+++ extension/src/popup.tsx
@@ -1,1 +1,22 @@
 // Placeholder
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(<Popup />, document.getElementById("root"));
+
+function Popup() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "8rem",
+      }}
+    >
+      A graduate project by @oenu, this is in beta.
+      <div></div>
+      <a href="https://github.com/oenu">github</a>
+      <div></div>
+      <a href="https://twitter.com/_a_nb">twitter</a>
+    </div>
+  );
+}

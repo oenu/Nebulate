7f5acd0e39fd50a1d8d2d43d88628c0d3c719a74
oenu <51684443+oenu@users.noreply.github.com>
2022-07-05 20:37:54 -0400

    added verbosity to response

--- src/routes/updateAll.ts
+++ src/routes/updateAll.ts
@@ -5,10 +5,26 @@ const app = express();
 
 import updateAll from "../batch/updateAll";
 
+// Mongo Models
+import { Channel } from "../models/channel/channel";
+
 app.put("/", async (_req: Request, res: Response) => {
-  logger.warn("updateAll: Updating all channels");
-  updateAll();
-  res.send("Updating all channels");
+  try {
+    const channels = await Channel.find({}).select(
+      "lastScrapedNebula lastScrapedYoutube lastMatched slug"
+    );
+    if (channels.length === 0) {
+      logger.error("UpdateAll: No channels found to update");
+      res.status(204).send("No channels found to update");
+    } else {
+      logger.info(`UpdateAll: Updating ${channels.length} channels`);
+      updateAll();
+      res.status(200).send("Updating all channels...");
+    }
+  } catch (err) {
+    logger.error(err);
+    res.status(500).send("Error updating channels");
+  }
 });
 
 module.exports = app;

6e053269ee18d6d0822b429613963b2599aeab0d
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-11-16 03:04:26 -0500

    added object hashing

--- src/table/uploadTable.ts
+++ src/table/uploadTable.ts
@@ -1,8 +1,10 @@
 import generateTable from "./generateTable";
+import type { LookupTable } from "./generateTable";
 import logger from "../utils/logger";
 import AWS from "aws-sdk";
 import fs = require("fs");
 import path = require("path");
+import hash from "object-hash";
 // Use AWS S3 to upload the table to cloudflare R2 CDN
 const uploadTable = async () => {
   // Check that env variables are set
@@ -12,11 +14,19 @@ const uploadTable = async () => {
   if (!process.env.SECRET_ACCESS_KEY) {
     throw new Error("SECRET_ACCESS_KEY not set");
   }
+  if (!process.env.R2_ENDPOINT) {
+    throw new Error("R2_ENDPOINT not set");
+  }
+  if (!process.env.R2_BUCKET_NAME) {
+    throw new Error("R2_BUCKET_NAME not set");
+  }
+  if (!process.env.R2_TABLE_KEY) {
+    throw new Error("R2_TABLE_KEY not set");
+  }
 
   // Create S3 client
   const s3 = new AWS.S3({
-    endpoint:
-      "https://97ba6ee2b2da3d74d5ab72746d725e97.r2.cloudflarestorage.com/neb-table",
+    endpoint: process.env.R2_ENDPOINT,
     accessKeyId: process.env.ACCESS_KEY_ID,
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
     signatureVersion: "v4",
@@ -30,7 +40,10 @@ const uploadTable = async () => {
     throw e;
   }
 
+  // Get the table to upload
   const lookupTablePath = path.join(__dirname, "/lookup_table.json");
+
+  // Check if the table exists - if it doesn't generate it
   if (!fs.existsSync(lookupTablePath)) {
     logger.warn(
       "uploadTable: lookup table does not exist, generating new table"
@@ -39,13 +52,57 @@ const uploadTable = async () => {
   }
 
   // Read the lookup table
-  const lookupTable = fs.readFileSync(lookupTablePath, "utf8");
+  const newTable = fs.readFileSync(lookupTablePath, "utf8");
+
+  // Convert the lookup table to a JSON object
+  const newTableObject: LookupTable = JSON.parse(newTable);
+
+  // Hash the lookup table without the generatedAt property or the id property
+  const newTableHash = hash(newTableObject, {
+    excludeKeys: (key) => key === "generatedAt" || key === "id",
+  });
+
+  // Get the table from cloudflare
+  const response = await s3
+    .getObject({
+      Bucket: "neb-table",
+      Key: "lookup_table.json",
+    })
+    .promise();
+
+  if (!response.Body) {
+    throw new Error("No body in response");
+  }
+
+  // If the table exists, check if the hash is the same
+  if (response.Body) {
+    const existingTableObject: LookupTable = JSON.parse(
+      response.Body.toString()
+    );
+    const existingTableHash = hash(existingTableObject, {
+      excludeKeys: (key) => key === "generatedAt" || key === "id",
+    });
+    if (existingTableHash === newTableHash) {
+      logger.info("uploadTable: Table is up to date");
+      return;
+    } else {
+      logger.info("uploadTable: Table is out of date, updating table...");
+    }
+  }
+
+  // Log the size of the table in bytes
+  logger.info(
+    `uploadTable: Uploading table with size ${Buffer.byteLength(
+      newTable,
+      "utf8"
+    )} bytes`
+  );
 
   // Upload the lookup table to cloudflare R2 CDN
   const params = {
-    Bucket: "neb-table",
-    Key: "lookup_table.json",
-    Body: lookupTable,
+    Bucket: process.env.R2_BUCKET_NAME,
+    Key: process.env.R2_TABLE_KEY,
+    Body: newTable,
   };
 
   s3.upload(params, function (err: Error, data: any) {

49a7d63cda9fa049b494ad17ff56e7046acaa9e7
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-11-24 05:29:45 -0500

    added url checks

--- extension/src/functions/checkTable.ts
+++ extension/src/functions/checkTable.ts
@@ -4,6 +4,7 @@ import { LookupTable } from "../parent_types";
 import { Video } from "../types";
 
 export const checkTable = async (url: string): Promise<Video | void> => {
+  console.log("Checking table for videoId: ", url);
   try {
     console.log("CheckTable: checking lookup table for video");
 
@@ -18,17 +19,34 @@ export const checkTable = async (url: string): Promise<Video | void> => {
       return;
     }
 
-    // Extract only the video ID using regex /(?<=[=\/&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#\n\r]|$)/
-    const videoId = url.match(
-      // /(?<=[=\/&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#\n\r]|$)/
-      /(?<=[=/&])[a-zA-Z0-9_-]{11}(?=[=/&?#\n\r]|$)/
-    )?.[0];
+    let videoId: string;
+    // Check if the video is a url or a videoId
+    if (url.includes("youtube.com")) {
+      // Extract only the video ID using regex /(?<=[=\/&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#\n\r]|$)/
+      const match = url.match(
+        // /(?<=[=\/&])[a-zA-Z0-9_\-]{11}(?=[=\/&?#\n\r]|$)/
+        /(?<=[=/&])[a-zA-Z0-9_-]{11}(?=[=/&?#\n\r]|$)/
+      )?.[0];
+      if (!match) {
+        console.warn("CheckTable: no videoId found in url");
+        return;
+      }
+      videoId = match;
+    } else {
+      videoId = url;
+    }
 
     console.log("CheckTable: videoId", videoId);
 
     // Check if the video ID exists
     if (!videoId) {
       console.debug(`CheckTable: could not extract video ID from URL ${url}`);
+      return;
+    }
+
+    //  Check Video ID is in the form of an 11 character string
+    if (videoId.length !== 11) {
+      console.warn(`CheckTable: video ID ${videoId} is not 11 characters long`);
       return;
     }
 

b8a1e65f0539b7d0cbd1898017d4e8b59f625d8d
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-11-25 10:41:03 -0500

    updated css rules

--- extension/src/contentFunctions/recommended/css.ts
+++ extension/src/contentFunctions/recommended/css.ts
@@ -3,27 +3,8 @@ import { Video } from "../../types";
 
 // eslint-disable-next-line no-undef
 export let styleElement: HTMLStyleElement | undefined; // The style element that is used to highlight videos on the page
-/**
- * styleUpdater: Create a style element and add it to the page with selectors for every video in the pageVideos object
- * 1. Create a style element if it doesn't exist
- * 1.1 Check the DOM for a style element, if it already exists, use that
- * 2. Create a selector for each videoId in the pageVideos object (use *= matching) (remove duplicates)
- * 2.1 If the video is matched (from a channel on nebula, and has a matching video on nebula) add to the matched selector
- * 2.2 If the video is known (is from a channel that is on nebula) add to the known selector
- * 2.3 If the video is unknown (is not from a channel that is on nebula) do nothing
- * 3. Add the selectors to the style element
- * 3.1 Matched Videos
- * Note: known selector (if option for known highlighting is enabled) also includes matched videos
- * 3.1.1 Add matched video styling - glow and add a nebulate icon in the top left corner
- * 3.2 Known Videos
- * 3.2.1 Combine Known video selectors
- * 3.2.1 Add known video styling - glow effect around the video card/div
- * Note: Known videos should have a glow effect, matched videos should have a glow effect
- * 4. Add the style element to the page
- * Note: we cant use ids because they are not unique (thanks google)
- * Note: videoId's are in the form of /watch?v=${videoId} or /watch?v=${videoId}&otherStuff or others
- */
-export const styleUpdater = async (
+
+export const createStyle = async (
   videos: Video[],
   options: {
     // Known
@@ -41,119 +22,111 @@ export const styleUpdater = async (
     matchedIconPosition?: "tl" | "tr" | "bl" | "br"; // The position of the icon to use for matched videos
     matchedIconColor?: string; // The hex color of the icon to use for matched videos
   } = {
+    // Known
     highlightKnown: true,
-    knownColor: "#AFE1AF", // Celadon
+    knownColor: "#FFD700", // Gold
+
+    // Matched
     highlightMatched: true,
-    matchedColor: " #FFD700", // Gold
+    // matchedColor: " #3EBBF3", // Gold
+    matchedColor: "#3EBBF3",
+
+    // Matched Icon
+    showMatchedIcon: true,
     matchedIcon: "https://via.placeholder.com/50",
     matchedIconSize: 20,
     matchedIconPosition: "tl",
-    matchedIconColor: "#000000",
+    matchedIconColor: "#3EBBF3",
   }
 ): Promise<void> => {
-  console.time("CS: styleUpdater");
-
-  // 1.
-  // Create a style element if it doesn't exist
+  // Create the style element if it doesn't exist
   if (!styleElement) {
-    // 1.1
-    // Check the DOM for a style element, if it already exists, use that
     // eslint-disable-next-line no-undef
-    styleElement = document.querySelector(
-      `#${CSS_IDS.BULK_VIDEO}`
-      // eslint-disable-next-line no-undef
-    ) as HTMLStyleElement;
-    if (!styleElement) {
-      // eslint-disable-next-line no-undef
-      styleElement = document.createElement("style");
-      styleElement.id = CSS_IDS.BULK_VIDEO;
-      // eslint-disable-next-line no-undef
-      document.head.append(styleElement);
-    }
+    styleElement = document.createElement("style");
+    styleElement.id = CSS_IDS.BULK_VIDEO;
+    // eslint-disable-next-line no-undef
+    document.head.appendChild(styleElement);
   }
 
-  // 2.
-  // Create a selector for each videoId in the pageVideos object (use *= matching) (remove duplicates)
-  const knownSelectors: string[] = [];
-  const matchedSelectors: string[] = [];
-  const knownVideoIds = new Set<string>();
-  const matchedVideoIds = new Set<string>();
-  videos.forEach((video) => {
-    // 2.1
-    // If the video is matched (from a channel on nebula, and has a matching video on nebula) add to the matched selector
-    if (video.matched) {
-      matchedVideoIds.add(video.videoId);
-      matchedSelectors.push(`[href*="/watch?v=${video.videoId}"]`);
-    }
+  const newStyle: string[] = [];
 
-    // 2.2
-    // If the video is known (is from a channel that is on nebula) add to the known selector
-    if (video.known) {
-      knownVideoIds.add(video.videoId);
-      knownSelectors.push(`[href*="/watch?v=${video.videoId}"]`);
-    }
-
-    // 2.3
-    // If the video is unknown (is not from a channel that is on nebula) do nothing
-    // Note: Unknown videos will not have a selector
-  });
-
-  // 3.
-  // Add the selectors to the style element
-  const styleText: string[] = [];
-
-  // 3.1
-  // Matched Videos
-  if (options.highlightMatched && matchedSelectors.length > 0) {
-    // Note: known selector (if option for known highlighting is enabled) also includes matched videos
-    // 3.1.1
-    // Add matched video styling - glow and add a nebulate icon in the top left corner
-    styleText.push(`
-      ${matchedSelectors.join(",")} {
-        ${
-          options.showMatchedIcon
-            ? `background-image: url(${options.matchedIcon});`
-            : ""
-        }
-        ${
-          options.showMatchedIcon
-            ? `background-position: ${options.matchedIconPosition};`
-            : ""
-        }
-        ${options.showMatchedIcon ? `background-repeat: no-repeat;` : ""}
-        ${
-          options.showMatchedIcon
-            ? `background-size: ${options.matchedIconSize}%;`
-            : ""
-        }
-        ${
-          options.showMatchedIcon
-            ? `background-color: ${options.matchedIconColor};`
-            : ""
-        }
-        box-shadow: 0 0 5px ${options.matchedColor};
-      }
-    `);
+  // Create the style for known videos
+  if (options.highlightKnown) {
+    const knownVideos = videos.filter((video) => video.matched && video.known);
+    const knownStyle = knownVideos
+      .map((video) => {
+        // Don't match the video if it is a playlist
+        return `
+        /* Known video: ${video.videoSlug} */
+        /* Search for the video title */
+        a[href*="${video.videoId}"]:not([href*="start_radio"]) #channel-name yt-formatted-string 
+        /* Set the color */
+        { color: ${options.knownColor} !important; }`;
+      })
+      .join("\n");
+    newStyle.push(knownStyle);
   }
 
-  // 3.2
-  // Known Videos
-  if (options.highlightKnown && knownSelectors.length > 0) {
-    // 3.2.1
-    // Add known video styling - glow effect around the video card/div
-    styleText.push(`
-      ${knownSelectors.join(",")} {
-        box-shadow: 0 0 5px ${options.knownColor};
-      }
-    `);
+  // Create the style for matched videos
+  if (options.highlightMatched) {
+    const matchedVideos = videos.filter((video) => video.matched);
+    const matchedStyle = matchedVideos.map((video) => {
+      // Channel Name
+
+      const channelName = `
+      /* Matched Channel: ${video.channelSlug} */
+      /* Search for matched channel name */
+        a[href*="${video.videoId}"]:not([href*="start_radio"]) #channel-name yt-formatted-string 
+        /* Set matched video's channel name to the matched color */
+        { color: ${options.matchedColor} !important }`;
+
+      // Video Title
+      const videoTitle = `
+      /* Search for matched video title */
+        a[href*="${video.videoId}"]:not([href*="start_radio"]) #video-title
+        /* Set matched video's title to the matched color */
+        { color: ${options.matchedColor} !important }`;
+
+      // Thumbnail
+      const thumbnail = `
+      /* Search for matched thumbnail */
+        a[href*="${video.videoId}"]:not([href*="start_radio"]).ytd-thumbnail
+        /* Set matched video's border to the matched color */
+        { border: 2px solid ${options.matchedColor} !important }`;
+
+      return [channelName, videoTitle, thumbnail].join("\n");
+    });
+
+    newStyle.push(matchedStyle.join("\n"));
   }
 
-  // 4.
-  // Add the style element to the page
-  styleElement.innerHTML = styleText.join("");
+  // Create the style for matched icons
+  // if (options.showMatchedIcon) {
+  //   const matchedVideos = videos.filter((video) => video.matched);
+  //   const matchedStyle = matchedVideos
+  //     .map((video) => {
+  //       // const iconPosition = options.matchedIconPosition;
+  //       const iconSize = options.matchedIconSize;
+  //       const iconColor = options.matchedIconColor;
+  //       const icon = options.matchedIcon;
+  //       return `
+  //         #thumbnail[href*="${video.videoId}"]+yt-image+img {
+  //           position: absolute;
+  //         }
+  //         #thumbnail[href*="${video.videoId}"]+yt-image+img {
+  //           content: url(${icon});
+  //           width: ${iconSize}%;
+  //           height: ${iconSize}%;
+  //           filter: invert(${iconColor});
+  //         }
+  //       }`;
+  //     })
+  //     .join("\n");
 
-  // Note: we cant use ids because they are not unique (thanks google)
-  // Note: videoId's are in the form of /watch?v=${videoId} or /watch?v=${videoId}&otherStuff or others
-  console.timeEnd("CS: styleUpdater");
-  return;
+  //   newStyle.push(matchedStyle);
+  // }
+
+  // Reset the style element
+  console.log("newStyle", newStyle);
+  styleElement.innerHTML = newStyle.join("\n");
 };

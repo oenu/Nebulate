0a7530eff2fe4b5bf5029f4c60c22bbb676e28de
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-11-25 18:19:15 -0500

    rewrote handler to use local function

--- extension/src/contentFunctions/recommended/handler.ts
+++ extension/src/contentFunctions/recommended/handler.ts
@@ -1,22 +1,25 @@
-import {
-  videoId,
-  CheckVideoMessage,
-  CheckVideoMessageResponse,
-} from "../../content_script";
-import { Messages } from "../../enums";
+import { checkTable } from "../../backgroundFunctions/checkTable";
+import { videoId } from "../../content_script";
 import { Video } from "../../types";
+import {
+  identifyPage,
+  YoutubePage,
+  // YoutubePageType
+} from "../page/identify";
 import { createStyle } from "./css";
 
 import { scrapeVideosFromPage } from "./scrape";
 
 // Local Video Cache:
 export let pageVideos: {
-  // A cache of video links/thumbnails on the page and their corresponding nebula video
   [key: string]: {
-    video: Video;
-    checked: boolean;
+    // videoId
+    video: Video; // Video object
+    checked: boolean; // Whether the video has been checked
   };
 };
+
+let localPage: YoutubePage;
 
 // BUG: Videos shown in the miniplayer are not detected, maybe switch all url matching to use the scrape/videoId function instead of the url
 
@@ -25,22 +28,32 @@ export let pageIntervalId: number; // The id of the interval that checks for new
  * Handle a new url being loaded
  * This should find all the links on the page that are known or matched videos and highlight them using css
  * 1. Clear the pageVideos cache
- * 2. Check for new videos using newVideosFromPage after 2 seconds (to allow the page to load)
+ * 1.1 Stop the interval that checks for new videos
+ * 2. Check for new videos using scrapeVideosFromPage after 2 seconds (to allow the page to load)
  * 3. Pass the new videos to handleNewVideos
  * 4. Check for new videos every 10 seconds (to handle infinite scroll)
  */
 export const urlUpdateHandler = async (url: string): Promise<void> => {
   console.debug("urlUpdateHandler: url update handler", url);
 
+  // Identify the page type
+  localPage = await identifyPage();
+
   // 1.
   // Clear the pageVideos cache
   pageVideos = {};
 
+  // 1.1
+  // Stop the interval that checks for new videos
+  if (pageIntervalId) {
+    clearInterval(pageIntervalId);
+  }
+
   // 2.
-  // Check for new videos using newVideosFromPage after 2 seconds
+  // Check for new videos using scrapeVideosFromPage after 2 seconds
   const newVideos = await new Promise<videoId[]>((resolve) => {
-    setTimeout(() => {
-      resolve(scrapeVideosFromPage());
+    setTimeout(async () => {
+      resolve(await scrapeVideosFromPage(localPage.type));
     }, 2000);
   });
 
@@ -49,25 +62,22 @@ export const urlUpdateHandler = async (url: string): Promise<void> => {
   console.debug("urlUpdateHandler: Passing new videos to HandleNewVideos");
   await handleNewVideos(newVideos);
 
-  // 4.
-  // Check for new videos every 10 seconds (to handle infinite scroll)
-  if (pageIntervalId) {
-    clearInterval(pageIntervalId);
-  }
-
   // HACK: for now max 2 times (for development purposes)
   let count = 0;
+
+  // 4.
+  // Check for new videos every 10 seconds (to handle infinite scroll)
   // eslint-disable-next-line no-undef
   pageIntervalId = window.setInterval(async () => {
     console.debug("urlUpdateHandler: Checking for new videos");
-    const newVideos = await scrapeVideosFromPage();
+    const newVideos = await scrapeVideosFromPage(localPage.type);
     if (newVideos.length > 0) {
       console.debug("urlUpdateHandler: New videos found");
       await handleNewVideos(newVideos);
     }
     count++;
     if (count > 2) {
-      console.debug("urlUpdateHandler: Stopping interval");
+      console.error("urlUpdateHandler: Dev: Stopping interval");
       clearInterval(pageIntervalId);
     }
   }, 10000);
@@ -76,8 +86,7 @@ export const urlUpdateHandler = async (url: string): Promise<void> => {
 /**
  * HandleNewVideos: Match videos, update pageVideos, trigger style updates
  * 1. Check if a list has been provided
- * 2. Send a message to the background script to check if the videos are known or matched
- * 2.1 Handle the response
+ * 2. Check the videos against the lookup table
  * 3. Update the pageVideos cache
  * 4. Use createStyle to update the styling for all videos in the pageVideos object
  */
@@ -90,37 +99,23 @@ export const handleNewVideos = async (newVideos: videoId[]): Promise<void> => {
   }
 
   // 2.
-  // Send a message to the background script to check if the videos are known or matched
-  console.debug("handleNewVideos: Sending message to background script");
-  const message: CheckVideoMessage = {
-    type: Messages.CHECK_VIDEO,
-    url: newVideos,
-  };
+  // Check the videos against the lookup table
+  console.time("handleNewVideos: checkTable");
+  checkTable(newVideos).then(async (response) => {
+    console.debug("handleNewVideos: Response received", response);
 
-  chrome.runtime.sendMessage(message, async (response) => {
-    // 2.1
-    // Handle the response
-    if (response) {
-      const { videos, type } = response as CheckVideoMessageResponse;
-
-      if (type !== Messages.CHECK_VIDEO_RESPONSE)
-        throw new Error("CS: handleNewVideos: invalid response type");
-      if (videos) {
-        // 3.
-        // Update the pageVideos cache
-        // Update the pageVideos object
-        console.debug("CS: handleNewVideos: updating pageVideos");
-
-        videos.forEach((video) => {
-          pageVideos[video.videoId] = { video, checked: true };
-        });
-
-        // 4.
-        // Use createStyle to update the styling for all videos in the pageVideos object
-        createStyle(Object.values(pageVideos).map((v) => v.video));
-      } else {
-        console.warn("CS: handleNewVideos: no videos found");
-      }
+    // 3.
+    // Update the pageVideos
+    for await (const video of response) {
+      pageVideos[video.videoId] = {
+        video,
+        checked: true,
+      };
     }
+
+    // 4.
+    // Use createStyle to update the styling for all videos in the pageVideos object
+    console.debug("handleNewVideos: creating style");
+    await createStyle(Object.values(pageVideos).map((video) => video.video));
   });
 };

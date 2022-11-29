73065b9568509043a62b5f3b3d727fdde9e47d26
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-11-29 08:57:35 -0500

    massive parallel speed boost

--- extension/src/content/listeners/onNewVideo.ts
+++ extension/src/content/listeners/onNewVideo.ts
@@ -8,6 +8,7 @@ import {
 } from "../handlers/interface/channelButton";
 import {
   addChannelStyle,
+  checkChannelStyle,
   removeChannelStyle,
 } from "../handlers/interface/channelStyle";
 import {
@@ -17,6 +18,7 @@ import {
 } from "../handlers/interface/videoButton";
 import {
   addVideoStyle,
+  checkVideoStyle,
   removeVideoStyle,
 } from "../handlers/interface/videoStyle";
 
@@ -40,27 +42,56 @@ export const onNewVideo = async (video: Video): Promise<void> => {
   console.time("onNewVideo");
   // Check if the video is new
   if (video === localVideo) {
-    // Existing video
-    if (video.matched)
-      // Check if video button needs to be re-added
-      (await checkVideoButton(video)) && (await addVideoButton(video));
+    // If video is matched, check if the video button and style exist. If channel is known, add the button and style
+    // Create array of promises
+    const promises = [];
+    if (video.matched) {
+      promises.push(
+        checkVideoButton(video).then((exists) => {
+          if (!exists) addVideoButton(video);
+        })
+      );
+      promises.push(
+        checkVideoStyle(video).then((exists) => {
+          if (!exists) addVideoStyle(video);
+        })
+      );
+    }
 
-    if (video.channel.known)
-      // Check if channel button needs to be re-added
-      (await checkChannelButton(video.channel)) &&
-        (await addChannelButton(video.channel));
+    if (video.channel.known) {
+      promises.push(
+        checkChannelButton(video.channel).then((exists) => {
+          if (!exists) addChannelButton(video.channel);
+        })
+      );
+      promises.push(
+        checkChannelStyle(video.channel).then((exists) => {
+          if (!exists) addChannelStyle(video.channel);
+        })
+      );
+    }
+
+    // Wait for all promises to resolve
+    if (promises.length > 0) {
+      console.time("onNewVideo: Promise.all");
+      await Promise.all(promises);
+      console.timeEnd("onNewVideo: Promise.all");
+    }
+  } else {
+    // New video
+    localVideo = video;
+    console.debug(
+      "onNewVideo: video is new, updating localVideo: ",
+      localVideo
+    );
+
+    // Check if the video is known - if so add the channel button and style channel box
+    video.known ? addChannelButton(video.channel) : removeChannelButton();
+    video.known ? addChannelStyle(video.channel) : removeChannelStyle();
+
+    // Check if the video is on Nebula - if so add the video button
+    video.matched ? addVideoButton(video) : removeVideoButton();
+    video.matched ? addVideoStyle(video) : removeVideoStyle();
+    console.timeEnd("onNewVideo");
   }
-
-  // New video
-  localVideo = video;
-  console.debug("onNewVideo: video is new, updating localVideo: ", localVideo);
-
-  // Check if the video is known - if so add the channel button and style channel box
-  video.known ? addChannelButton(video.channel) : removeChannelButton();
-  video.known ? addChannelStyle(video.channel) : removeChannelStyle();
-
-  // Check if the video is on Nebula - if so add the video button
-  video.matched ? addVideoButton(video) : removeVideoButton();
-  video.matched ? addVideoStyle(video) : removeVideoStyle();
-  console.timeEnd("onNewVideo");
 };

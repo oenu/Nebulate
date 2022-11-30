4a202d1f4299b14543b3e16936cc16918597e20e
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-11-30 14:52:03 -0500

    improved logging

--- src/batch/checkAllConfigs.ts
+++ src/batch/checkAllConfigs.ts
@@ -1,4 +1,3 @@
-import type { discrepancy } from "../models/channel/methods/checkConfig";
 // rechecks channel information from nebula and youtube, provides a summary of any changes
 
 import { Channel } from "../models/channel/channel";
@@ -15,29 +14,26 @@ export const checkAllConfigs = async (): Promise<void> => {
     throw new Error("No channels found");
   }
 
-  const channelDiscrepancies: { [key: string]: discrepancy }[] = [];
-
   // Loop through each channel and call the checkConfig method
   for await (const channel of channels) {
     await channel
       .checkConfig()
       .then((discrepancy) => {
-        channelDiscrepancies.push(discrepancy);
         if (!discrepancy) {
           logger.info(`Channel ${channel.slug} is up to date`);
+        } else {
+          // logger.info (`[${discrepancy[key]} : ${discrepancy[key]?.platform}] ${key} has changed from ${value.db_value} to ${value.api_value}`)
+          if (discrepancy) {
+            for (const [key, value] of Object.entries(discrepancy)) {
+              logger.info(
+                `[${discrepancy[key]?.field} : ${discrepancy[key]?.platform}] ${key} has changed from ${value.db_value} to ${value.api_value}`
+              );
+            }
+          }
         }
       })
       .catch((err) => {
         logger.error(err);
       });
   }
-
-  // Log the results in a readable format to the verbose console
-  logger.verbose(`
-    ${channelDiscrepancies.length} channels had discrepancies out of ${channels.length} total channels`);
-  channelDiscrepancies.forEach((discrepancy) => {
-    logger.verbose(
-      `[${discrepancy.platform}:${discrepancy.field}] (Stored) ${discrepancy.db_value} !== ${discrepancy.api_value} (api)`
-    );
-  });
 };

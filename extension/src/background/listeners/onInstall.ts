2623bd8d9c136fbdb06495bff7cfc0055ed0cb95
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-12-01 16:25:31 -0500

    removed outdated options handler

--- extension/src/background/listeners/onInstall.ts
+++ extension/src/background/listeners/onInstall.ts
@@ -1,4 +1,3 @@
-import { defaults } from "../../common/defaults";
 import { updateTable } from "../table/updateTable";
 
 /**
@@ -15,37 +14,37 @@ chrome.runtime.onInstalled.addListener(async function () {
   // 1.
   // Set default preferences
   try {
-    // 1.1
-    // Set pref for opening Nebula links in a new tab
-    const storage = await chrome.storage.sync.get("preferNewTab");
-    if (storage.preferNewTab === undefined) {
-      chrome.storage.sync.set({ preferNewTab: defaults.preferNewTab });
-    }
+    // // 1.1
+    // // Set pref for opening Nebula links in a new tab
+    // const storage = await chrome.storage.sync.get("preferNewTab");
+    // if (storage.preferNewTab === undefined) {
+    //   chrome.storage.sync.set({ preferNewTab: defaults.preferNewTab });
+    // }
 
-    // 1.2
-    // Set pref for showing the channel button
-    const showChannelButton = await chrome.storage.sync.get(
-      "showChannelButton"
-    );
-    if (showChannelButton.showChannelButton === undefined) {
-      chrome.storage.sync.set({
-        showChannelButton: defaults.showChannelButton,
-      });
-    }
+    // // 1.2
+    // // Set pref for showing the channel button
+    // const showChannelButton = await chrome.storage.sync.get(
+    //   "showChannelButton"
+    // );
+    // if (showChannelButton.showChannelButton === undefined) {
+    //   chrome.storage.sync.set({
+    //     showChannelButton: defaults.showChannelButton,
+    //   });
+    // }
 
-    // 1.3
-    // Set pref for showing the video button
-    const showVideoButton = await chrome.storage.sync.get("showVideoButton");
-    if (showVideoButton.showVideoButton === undefined) {
-      chrome.storage.sync.set({ showVideoButton: defaults.showVideoButton });
-    }
+    // // 1.3
+    // // Set pref for showing the video button
+    // const showVideoButton = await chrome.storage.sync.get("showVideoButton");
+    // if (showVideoButton.showVideoButton === undefined) {
+    //   chrome.storage.sync.set({ showVideoButton: defaults.showVideoButton });
+    // }
 
-    // 1.4
-    // Set pref for time to wait before refreshing the lookup table
-    const updateInterval = await chrome.storage.sync.get("updateInterval");
-    if (updateInterval.updateInterval === undefined) {
-      chrome.storage.sync.set({ updateInterval: defaults.updateInterval });
-    }
+    // // 1.4
+    // // Set pref for time to wait before refreshing the lookup table
+    // const updateInterval = await chrome.storage.sync.get("updateInterval");
+    // if (updateInterval.updateInterval === undefined) {
+    //   chrome.storage.sync.set({ updateInterval: defaults.updateInterval });
+    // }
 
     // 2
     // Set whether the extension is in development mode

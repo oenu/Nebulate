abdec7ba17e73c148553f048aac867bc7e48ccfb
Adam Newton-Blows <51684443+oenu@users.noreply.github.com>
2022-12-09 07:36:10 -0500

    split directories

--- extension/src/background.ts
+++ extension/src/background.ts
@@ -1,5 +1,26 @@
 console.log("Background: Running background script");
 
+// Detect if running in Firefox
+console.log("Background: Detecting browser from manifest");
+const manifest = chrome.runtime.getManifest();
+console.log(manifest);
+// Check if the browser is Firefox
+let isFirefox = false;
+try {
+  if (!manifest?.minimum_chrome_version) {
+    console.log("Background: Browser is not Chrome, assuming Firefox");
+    isFirefox = true;
+  } else {
+    console.log("Background: Browser is Chrome");
+  }
+} catch (e) {
+  console.log("Background: Error detecting browser, assuming Firefox");
+  isFirefox = true;
+}
+
+// Set the browser name
+export const browserName = isFirefox ? "firefox" : "chrome";
+
 // Alarms
 console.log("Background: Loading alarm module");
 import "./background/alarms/updateTableAlarm";
